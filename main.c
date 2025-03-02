c9bb9bd07675d08ce6439ae36497fa1a61cc55b5
Vladyslav Umerenko <space.vlad7@gmail.com>
2025-03-02 20:27:49 +0200

    Blink built-in LED light when button is pressed

--- main.c
+++ main.c
@@ -2,12 +2,20 @@
 #include "avr/io.h"
 #include "util/delay.h"
 
+// Input PD2
+// Output PB5
+
 int main(void) {
-  DDRB |= (1 << PB5);
+  DDRD &= ~(1 << PD2); // set input mode to arduino pin 2
+  DDRB |= (1 << PB5); // set output mode to arduino pin 13
+  PORTD |= (1 << PD2); // Enable pull-up resistor, PD2 is HIGH by default -> button unpressed
 
   while (1) {
-    PORTB ^= (1 << PB5);
-    _delay_ms(100);
+    if (!(PIND & (1 << PD2))) { // button pressed
+      PORTB &= ~(1 << PB5); // turn on
+    } else {
+      PORTB |= (1 << PB5); // turn off
+    }
   }
 
   return 0;
